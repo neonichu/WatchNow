9c2a27ca65d88bfb9f769b901b0a27802c80f709
Boris Bügling <boris@icculus.org>
2013-05-17 09:55:49 +0200

    Initial Commit

---
+++ WatchNow/BBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBUAppDelegate.h
+//  WatchNow
+//
+//  Created by Boris Bügling on 17.05.13.
+//  Copyright (c) 2013 Boris Bügling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
